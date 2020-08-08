bf0f9f1b0b8cbc402aa1748598953a9101b65384
sijunelee <sijune0525@gmail.com>
2020-08-08 17:42:52 +0900

    Updates

---
+++ precache-manifest.a66fa65dc34fc65c05aea37c0086f732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec28bd71fe336c44527ba368b512894",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1c255640cacfbbba8197",
+    "url": "/movie_app_2020/static/css/main.8132f1fc.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c255640cacfbbba8197",
+    "url": "/movie_app_2020/static/js/main.68d510d5.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
